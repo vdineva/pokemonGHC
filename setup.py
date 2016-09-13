17e0160c5401563fc3046f6dc53f44e05cfd1dfc
Melissa Kam <melissa.kam@rackspace.com>
2016-09-13 10:07:26 -0500

    Adding a basic setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='pokestarter',
+      version='0.1',
+      description='API and UI for deciding which Pokemon starter to choose',
+      url='https://github.com/BeenzSyed/pokemonGHC',
+      packages=['pokestarter'],
+      install_requires=['Flask==0.11'])
