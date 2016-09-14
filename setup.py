5bd2cc8cfac2249f121e61ebe49d9b6f6d697fc3
Melissa Kam <melissa.kam@rackspace.com>
2016-09-14 14:34:35 -0500

    Including UI files to installation

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='pokestarter',
       description='API and UI for deciding which Pokemon starter to choose',
       url='https://github.com/BeenzSyed/pokemonGHC',
       packages=['pokestarter'],
+      include_package_data=True,
       install_requires=['Flask==0.11'])
