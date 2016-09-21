7dfe8861c1055da369aa4701a73c9a971e8fc992
Melissa Kam <melissa.kam@rackspace.com>
2016-09-21 17:32:58 -0500

    Updating deploy scripts

--- deploy_to_test_env.sh
+++ deploy_to_test_env.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cp -r pokestarter /pokestarter
+cp -r pokestarter/static /pokestarter/static
+cp -r pokestarter/templates /pokestarter/templates
+cp pokestarter/app.py /pokestarter/app.py
+cp pokestarter/starters.py /pokestarter/starters.py
