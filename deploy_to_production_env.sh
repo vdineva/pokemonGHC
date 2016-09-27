36dee7fcfe355d200c03512023f694d8f924c7fa
Melissa Kam <melissa.kam@rackspace.com>
2016-09-27 13:12:55 -0500

    Updating deploy script to use release artifact

--- deploy_to_production_env.sh
+++ deploy_to_production_env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cp -r pokestarter/static /pokestarter/pokestarter/static
-cp -r pokestarter/templates /pokestarter/pokestarter/templates
-cp pokestarter/app.py /pokestarter/pokestarter/app.py
-cp pokestarter/starters.py /pokestarter/pokestarter/starters.py
+RELEASE_ARTIFACT=$1
+tar -xvf $RELEASE_ARTIFACT.tar.gz
+
+cp -r $RELEASE_ARTIFACT/pokestarter/static /pokestarter_prod/pokestarter
+cp -r $RELEASE_ARTIFACT/pokestarter/templates /pokestarter_prod/pokestarter
+cp $RELEASE_ARTIFACT/pokestarter/app.py /pokestarter_prod/pokestarter/app.py
+cp $RELEASE_ARTIFACT/pokestarter/starters.py /pokestarter_prod/pokestarter/starters.py
