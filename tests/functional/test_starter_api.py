7cde8e7c987bdabdcf02656e43ace0b6c7ecffc6
Melissa Kam <melissa.kam@rackspace.com>
2016-09-29 10:26:27 -0500

    Increasing verification attempts in functional tests

--- tests/functional/test_starter_api.py
+++ tests/functional/test_starter_api.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+import time
 import unittest
 
 
@@ -8,12 +9,15 @@ class TestPokemon(unittest.TestCase):
 
     def test_get_starter_api(self):
         url = os.environ.get("TEST_URL") or "http://localhost:5000/api"
-        response = requests.get(url)
-        self.assertEquals(response.status_code, 200)
-        json_data = json.loads(response.content)
 
-        starter = json_data.get("pokemon")
-        self.assertIsNotNone(starter)
+        for i in range(30):
+            response = requests.get(url)
+            self.assertEquals(response.status_code, 200)
+            json_data = json.loads(response.content)
 
-        starters = ["Charmander", "Squirtle", "Bulbasaur"]
-        self.assertIn(starter, starters)
+            starter = json_data.get("pokemon")
+            self.assertIsNotNone(starter)
+
+            starters = ["Charmander", "Squirtle", "Bulbasaur"]
+            self.assertIn(starter, starters)
+            time.sleep(1)
