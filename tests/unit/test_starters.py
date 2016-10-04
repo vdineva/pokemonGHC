e93c9155db109ba26c1e7fed723f6dd7f0a4cf87
Melissa Kam <melissa.kam@rackspace.com>
2016-10-04 16:09:19 -0500

    Update test_starters.py

--- tests/unit/test_starters.py
+++ tests/unit/test_starters.py
@@ -5,7 +5,7 @@ import unittest
 class TestPokemon(unittest.TestCase):
 
     def test_get_random_starters(self):
-        starters = ["Charmander", "Squirtle", "Bulbasaur"]
+        starters = ["Charmander", "Squirtle", "Bulbasaur", "Pikachu"]
 
         for i in range(0, 50):
             starter = get_random_starter()
